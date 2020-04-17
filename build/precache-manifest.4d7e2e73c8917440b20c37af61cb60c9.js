c2ff3303dd65e14100a6d579ca9a581876f7acc0
Lucas Barcelos <lucasbarcelos58@gmail.com>
2020-04-17 00:24:13 -0300

    it's works?

---
+++ build/precache-manifest.4d7e2e73c8917440b20c37af61cb60c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b853063aa427c85f1ef70b77c248a57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "571da0a7ec8bb0fc226b",
+    "url": "/static/css/main.b2d36835.chunk.css"
+  },
+  {
+    "revision": "33bd7ed8659c50cfebec",
+    "url": "/static/js/2.9919bfcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9919bfcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571da0a7ec8bb0fc226b",
+    "url": "/static/js/main.df3592b2.chunk.js"
+  },
+  {
+    "revision": "ebe2778410a5267872a5",
+    "url": "/static/js/runtime-main.ccb80219.js"
+  },
+  {
+    "revision": "d2a9be113a68a7ec4f89a9774d50ac2a",
+    "url": "/static/media/VJ_LOOP_004_-_2200_hrs_4K_hd.d2a9be11.mp4"
+  }
+]);

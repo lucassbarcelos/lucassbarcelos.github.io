430b16bcd016c8d9a59376fdd0d98304ca22aeb6
Lucas Barcelos <lucasbarcelos58@gmail.com>
2020-04-17 00:14:26 -0300

    criando interface inicial

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,33 @@
 import React from "react";
-import Post from "./Post";
+import { FaGithub } from "react-icons/fa";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Post
-          title="VAMOS VER SE ROLA"
-          comment="testando pra ver se rola mesmo "
-        />
-        <Post title="VAMOS VER SE ROLA" />
-        <Post title="VAMOS VER SE ROLA" />
+import "./BackgroundVideo.css";
+
+import videos from "../VJ_LOOP_004_-_2200_hrs_4K_hd.mp4";
+
+const BackgroundVideo = () => {
+  return (
+    <div className="Container">
+      <video autoPlay="autoplay" loop="loop" muted className="Video">
+        <source src={videos} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="Content">
+        <div className="SubContent">
+          <img
+            src="https://avatars2.githubusercontent.com/u/13748415?s=460&u=8fd252c46e97a8e711d6f17fa6a3e379eb38de2f&v=4"
+            alt="profile"
+          />
+          <h1>Lucas Barcelos</h1>
+          <p>Web developer using React</p>
+          <button type="button" className="btn btn-outline-dark">
+            <FaGithub size={18} color="#FFF" /> Github
+          </button>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default BackgroundVideo;

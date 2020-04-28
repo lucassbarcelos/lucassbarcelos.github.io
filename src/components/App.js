575226f1aaab84a2806476be08f230a9e30f40ed
Lucas Barcelos <lucasbarcelos58@gmail.com>
2020-04-28 00:20:46 -0300

    update: i need learn more css

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGithub, FaUserNinja } from "react-icons/fa";
+import { FaGithub, FaUserNinja, FaAngleDoubleDown } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 import "./BackgroundVideo.css";
@@ -20,10 +20,11 @@ const BackgroundVideo = () => {
             alt="profile"
           />
           <button type="button" className="teste btn btn-outline-dark ">
-            <FaUserNinja size={18} color="#FFF" /> About.Me
+            <FaUserNinja size={20} color="#FFF" /> About.Me
           </button>
           <button type="button" className="btn btn-outline-dark">
             <FaGithub size={18} color="#FFF" />
+            <b>Jobs</b>
           </button>
           <button type="button" className="btn btn-outline-dark">
             <FaGithub size={18} color="#FFF" /> Github
@@ -32,24 +33,23 @@ const BackgroundVideo = () => {
 
         <div className="Content">
           <div className="SubContent">
-            <div className="SubContentC">
-              <img
-                src="https://avatars2.githubusercontent.com/u/13748415?s=460&u=8fd252c46e97a8e711d6f17fa6a3e379eb38de2f&v=4"
-                alt="profile"
-              />
-              <h1>Lucas Barcelos</h1>
-              <p>Web developer using React</p>
-              <Link To="https://github.com/lucassbarcelos">
-                <button
-                  type="button"
-                  onClick="redirect('github.com/lucassbarcelos')"
-                  className="btn btn-outline-dark"
-                >
-                  <FaGithub size={18} color="#FFF" /> Github
-                </button>
-              </Link>
-            </div>
+            <img
+              src="https://avatars2.githubusercontent.com/u/13748415?s=460&u=8fd252c46e97a8e711d6f17fa6a3e379eb38de2f&v=4"
+              alt="profile"
+            />
+            <h1>Lucas Barcelos</h1>
+            <p>Web developer using React</p>
+            <Link To="https://github.com/lucassbarcelos">
+              <button
+                type="button"
+                onClick="redirect('github.com/lucassbarcelos')"
+                className="btn btn-outline-dark"
+              >
+                <FaGithub size={18} color="#FFF" /> Github
+              </button>
+            </Link>
           </div>
+          <FaAngleDoubleDown className="next" size={100} color="#BBB" />
         </div>
       </div>
       <div id="aboutMe">teste</div>
